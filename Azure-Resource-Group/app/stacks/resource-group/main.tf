b0fd78c26daa6c4f39f5afc3e7587c91f72d2fe6
ahmetaslan13 <ahmetaslan.work@gmail.com>
2022-10-19 12:47:34 -0400

    added az-resource-group

---
+++ Azure-Resource-Group/app/stacks/resource-group/main.tf
@@ -0,0 +1,7 @@
+// *** Create Resource Group Values ***
+module "resource_group" {
+  source              = "../../modules/resource-group"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  tags                = var.tags
+}
